090b2aa29bf4e2b9f72959409f34319f759741c1
“Okabe-15” <cheridalihadi@gmail.com>
2026-09-18 02:05:39 +0700

    Add, delete dan edit sudah selesai

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -2,6 +2,11 @@ const router = require('express').Router()
 const AdminController = require('../controllers/adminController')
 
 router.get('/', AdminController.showHomestaysAdmin)
+router.get('/homestays/add', AdminController.getAddHomestay)
+router.post('/homestays/add', AdminController.postAddHomestay)
+router.get('/homestays/:id/edit', AdminController.getEditHomestay)
+router.post('/homestays/:id/edit', AdminController.postEditHomestay)
+router.post('/homestays/:id/delete', AdminController.deleteHomestay)
 
 
 module.exports = router
