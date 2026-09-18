6437097b3fa4b94180e5fca00d8eef4606ad0d70
Muhamad Yoga Supratama <muhamadyogasupratama@gmail.com>
2026-09-18 10:51:03 +0700

    doneAfterPresent

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -57,6 +57,86 @@ class AdminController {
       res.send(error);
     }
   }
+
+  static async getAddHomestay(req, res) {
+    try {
+      res.render("addFormHomestay");
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async postAddHomestay(req, res) {
+    try {
+      const { name, imageUrl, pricePerNight, roomAvailable, contactPIC } =
+        req.body;
+
+      await Homestay.create({
+        name,
+        imageUrl,
+        pricePerNight,
+        roomAvailable,
+        contactPIC,
+      });
+
+      res.redirect("/admin");
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async getEditHomestay(req, res) {
+    try {
+      const { id } = req.params;
+      const homestay = await Homestay.findByPk(id);
+      res.render("editFormHomestay", { homestay });
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async postEditHomestay(req, res) {
+    try {
+      const { id } = req.params
+
+      const { name, imageUrl, pricePerNight, roomAvailable, contactPIC } =
+        req.body;
+
+      await Homestay.update(
+        {
+          name,
+          imageUrl,
+          pricePerNight,
+          roomAvailable,
+          contactPIC,
+        },
+        {
+          where: {
+            id: id,
+          },
+        },
+      );
+
+      res.redirect("/admin");
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async deleteHomestay(req, res) {
+    try {
+      const { id } = req.params;
+
+      await Homestay.destroy({
+        where: { id },
+      });
+
+      res.redirect('/admin');
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
 }
 
 module.exports = AdminController;
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -2,6 +2,10 @@ const router = require('express').Router()
 const AdminController = require('../controllers/adminController')
 
 router.get('/', AdminController.showHomestaysAdmin)
-
+router.get('/homestays/add', AdminController.getAddHomestay)
+router.post('/homestays/add', AdminController.postAddHomestay)
+router.get('/homestays/:id/edit', AdminController.getEditHomestay)
+router.post('/homestays/:id/edit', AdminController.postEditHomestay)
+router.get('/homestays/:id/delete', AdminController.deleteHomestay)
 
 module.exports = router
